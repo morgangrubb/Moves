d5a1bc50c760697048be42a354945bac88928d65
Morgan Grubb <morgan@wishpond.com>
2011-01-16 18:38:28 -0800

    Initial commit of Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moves::Application.config.secret_token = '17efb13b8dcb27001d195ec2a0556061938bca32a50649f3e92b70df3d3e1f520973caa094e798b72064ff993aef92aeff6785f36dfecdf264654b3969b73af2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Moves::Application.config.session_store :cookie_store, :key => '_moves_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Moves::Application.config.session_store :active_record_store
